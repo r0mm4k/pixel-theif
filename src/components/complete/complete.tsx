5bcd1ad9f7f93c7f6bc0f0beb72755216846eb99
Roma Makarov <r0mmm4k@gmail.com>
2021-08-30 17:31:57 +0300

    - fix stories

--- src/components/complete/complete.tsx
+++ src/components/complete/complete.tsx
@@ -4,7 +4,7 @@ import { Button, EImageSizes, Heading, Image, Menu } from '@/components';
 import treasure from '@/assets/images/treasure.png';
 import { ICompleteProps } from './complete.types';
 
-const Complete: FC<ICompleteProps> = ({ onGoHomef }) => {
+const Complete: FC<ICompleteProps> = ({ onGoHome }) => {
   return (
     <Menu>
       <Heading>Quest Complete!</Heading>
