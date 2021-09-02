e2e19c2f012b34c32cec61bd5d4a317fd8927a9d
Roma Makarov <r0mmm4k@gmail.com>
2021-09-02 18:24:51 +0300

    - add player movement

--- src/machines/player/player.machine.ts
+++ src/machines/player/player.machine.ts
@@ -1,25 +1,47 @@
-import { createMachine } from 'xstate';
-import { log } from 'xstate/lib/actions';
+import { assign, createMachine } from 'xstate';
+import { choose } from 'xstate/lib/actions';
 
 import { PLAYER_STARTING_COORDS } from '@/constants';
+import { getTargetCoords, isAllowCoords } from '@/utils';
 import { IPlayerContext, IPlayerState, TPlayerEvent } from '.';
 
-const playerMachine = createMachine<IPlayerContext, TPlayerEvent, IPlayerState>({
-  id: 'player',
-  context: {
-    coords: PLAYER_STARTING_COORDS,
-  },
-  initial: 'alive',
-  states: {
-    alive: {
-      on: {
-        ARROW_BUTTON_CLICKED: {
-          actions: log(),
+const playerMachine = createMachine<IPlayerContext, TPlayerEvent, IPlayerState>(
+  {
+    id: 'player',
+    context: {
+      coords: PLAYER_STARTING_COORDS,
+    },
+    initial: 'alive',
+    states: {
+      alive: {
+        on: {
+          ARROW_BUTTON_CLICKED: {
+            actions: 'onArrowButtonClicked',
+          },
         },
       },
+      dead: {},
     },
-    dead: {},
   },
-});
+  {
+    actions: {
+      onArrowButtonClicked: choose([{ cond: 'isAllowMove', actions: 'move' }]),
+      move: assign(({ coords }, { direction }) => {
+        const targetCoords = getTargetCoords({ coords, direction });
+
+        return { coords: targetCoords };
+      }),
+    },
+    guards: {
+      isAllowMove: ({ coords }, { type, direction }) => {
+        if (type !== 'ARROW_BUTTON_CLICKED') return false;
+
+        const targetCoords = getTargetCoords({ coords, direction });
+
+        return isAllowCoords(targetCoords);
+      },
+    },
+  }
+);
 
 export { playerMachine };
--- src/utils/coords.ts
+++ src/utils/coords.ts
@@ -1,7 +1,37 @@
+import { EDirection } from '@/enums';
 import { TCoords } from '@/types';
 import { GRID_SQUARE_SIZE } from '@/constants';
+import { grid } from './grid';
+import { isEqual } from 'lodash';
 
 const coordsToPosition = (coords: TCoords) =>
   coords.map((coord) => `${coord * GRID_SQUARE_SIZE}px`);
 
-export { coordsToPosition };
+interface IGetTargetCoordsProps {
+  coords: TCoords;
+  direction: EDirection;
+}
+
+const getTargetCoords = ({ coords, direction }: IGetTargetCoordsProps): TCoords => {
+  switch (direction) {
+    case EDirection.up: {
+      return [coords[0], coords[1] - 1];
+    }
+    case EDirection.down: {
+      return [coords[0], coords[1] + 1];
+    }
+    case EDirection.left: {
+      return [coords[0] - 1, coords[1]];
+    }
+    case EDirection.right: {
+      return [coords[0] + 1, coords[1]];
+    }
+    default: {
+      throw new Error(`Unknown direction: ${direction}`);
+    }
+  }
+};
+
+const isAllowCoords = (coords: TCoords) => grid.some((gridCoords) => isEqual(gridCoords, coords));
+
+export { coordsToPosition, getTargetCoords, isAllowCoords };
